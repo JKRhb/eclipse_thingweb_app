93808cef928904bc154dbd50215f6b153ad5d082
Jan Romann <jan.romann@hs-emden-leer.de>
2024-10-28 11:34:28 +0100

    fixup! feat: implement first version of app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,9 @@
+// Copyright 2024 Contributors to the Eclipse Foundation. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
 import 'package:dart_wot/core.dart';
 import 'package:eclipse_thingweb_app/main.dart';
 import 'package:flutter/material.dart';
